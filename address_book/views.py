dabf5af83a02b596e5335c671a4164426f07403b
Courtney Jacobs <cjacobs124@gmail.com>
2020-04-26 19:54:48 -0400

    All Addresses

--- address_book/views.py
+++ address_book/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Address 
 
 # Create your views here.
 
 # HOME PAGE
 def home(request):
-	return render(request, 'home.html', {})
+	all_addresses = Address.objects.all
+	return render(request, 'home.html', {'all_addresses': all_addresses})
 
 # ADD ADDRESS PAGE
 def add_address(request):
